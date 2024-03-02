f40467b08a2950ebb13cf93e35f356ea1347a8c3
stefan jonsson <stepson_jonsson@hotmail.com>
2024-03-02 19:10:32 +0100

    feat: add strings.py

---
+++ strings.py
@@ -0,0 +1,5 @@
+hello = 'Hello' ## strings can be made with single or double quotes
+name = input("What is your name?\n") ## input function is similar to Console.ReadLine();
+helloWorld = hello + " " + name ## string concatenation is same as c#
+
+print(helloWorld) 
