cb2d6ac174f2cd7eea424f098c4c72f88849244e
stefan jonsson <stepson_jonsson@hotmail.com>
2024-03-03 14:47:41 +0100

    feat: add conditionals.py

---
+++ conditionals.py
@@ -0,0 +1,15 @@
+print(1 < 2) ## conditionals are same as c#
+print(1 <= 2)
+print(1 == 2)
+print(1 >= 2)
+print(1 > 2)
+print(1 != 2)
+
+if (1 < 2): ## You use indentation instead of curly brackets for code blocks.
+    print("1 is less than 2")
+
+if (1 < 2 and 1 == 1): ## logical operators use keywords instead of symbols, for example and instead of &&
+    print("true")
+
+if (not 1 < 2): ## you would use the not keyword instead of ! symbol to negate a condition
+    print("something")
