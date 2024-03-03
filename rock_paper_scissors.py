8982432daae20051dcfc6c0bd289de619cedd45e
stefan jonsson <stepson_jonsson@hotmail.com>
2024-03-03 15:37:36 +0100

    chore: ensure correct input from user in rock_aperp_scissors.py

--- rock_paper_scissors.py
+++ rock_paper_scissors.py
@@ -1,16 +1,29 @@
 import random
 
-computer_choise = random.choice(['rock', 'paper', 'scissors'])
-user_choice = input("rock, paper, or scissors?")
+
+valid_options = ['rock', 'paper', 'scissors']
+
+
+computer_choise = random.choice(valid_options) 
+
+while True:
+    user_choice = input("rock, paper, or scissors?").lower() 
+
+    if user_choice not in valid_options:
+        print("Invalid input, please pick rock, paper or scissors.")
+        continue
+
+    break
+
 win = 'YOU WIN'
 loose = 'YOU LOOSE'
 tie = 'YOU TIE'
-reason = "because the computer picked " + computer_choise
+reason = " because the computer picked " + computer_choise
 
 if computer_choise == user_choice:
     print(tie + reason)
 elif user_choice == 'rock' and computer_choise == 'scissors':
-    print(win +  reason)
+    print(win + reason)
 elif user_choice == 'paper' and computer_choise == 'rock':
     print(win + reason)
 elif user_choice == 'scissors' and computer_choise == 'paper':
