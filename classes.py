c10820038830a387414cfb12c4d828eb15ebe3fc
stefan jonsson <stepson_jonsson@hotmail.com>
2024-03-10 17:14:45 +0100

    feat: add some code regarding inheritance in classes.py

--- classes.py
+++ classes.py
@@ -7,9 +7,20 @@ class Robot_Dog:
     def bark(self):
         print('Woof woof!')
 
+class InheritanceTest(Robot_Dog): ## To inherit a class put it in ()
+
+    def bark(self): ## You can override a method by just defining it again
+        print('Overridden Woof woof')
+    
 myDog = Robot_Dog('Diezel', 'Rottweiler')
+myDog2 = InheritanceTest('Diezel', 'Rottweiler') ## We inherited the __init__ method so need to write our own one.
 
 print(myDog.breed)
 print(myDog.name)
-
 myDog.bark()
+
+print(myDog2.breed)
+print(myDog2.name)
+
+
+myDog2.bark()
